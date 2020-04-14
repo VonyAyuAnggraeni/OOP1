3c1b110a7e8755f1261e8ce2bc1f690978d23c05
VonyAyuAnggraeni <57248883+VonyAyuAnggraeni@users.noreply.github.com>
2020-04-14 14:15:35 +0700

    Add files via upload

---
+++ Tugas1_revisi/Aplikasi.java
@@ -0,0 +1,7 @@
+public class Aplikasi{
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa("18090100", "Vony Ayu Anggraeni", "4D");
+        System.out.println(mhs.info());
+        
+    }
+}
